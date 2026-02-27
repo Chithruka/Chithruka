680ccefe6eae59fda05d1cadfbcae8c92d16242f
Chithruka Sri Yudam Nanayakkara <chithrukasri@gmail.com>
2026-02-27 22:16:02 +0530

    Add files via upload

--- dubbed-registry.js
+++ dubbed-registry.js
@@ -1,8 +1,7 @@
-// dubbed-registry.js
 const DUBBED_REGISTRY = {
     "597": {
         type: "movie",
-        title: "Titanic",
+        title: "Titanic (1997)",
         links: [
             { language: "Sinhala", quality: "720p", size: "1.2GB", url: "https://server-dubhublk.cloudew.workers.dev/1:/Remux/Dub/Titanic.1997.Open.Matte.720p.BluRay.x264-DubZoneLK.mkv" },
             { language: "Sinhala", quality: "1080p", size: "2.1GB", url: "https://server-dubhublk.cloudew.workers.dev/1:/Remux/Dub/Titanic.1997.Open.Matte.1080p.BluRay.x264.2CH-DubZoneLK.mkv" },
@@ -11,37 +10,124 @@ const DUBBED_REGISTRY = {
     },
     "7704": {
         type: "tv",
-        title: "Legend of the Seeker",
+        title: "Legend of the Seeker (2008)",
         links: [
             { language: "Sinhala", quality: "S1 | 720p", size: "400MBx22", url: "https://mega.nz/folder/goNXGAzJ#RunQgaWWLOeCv2CyOuCnRg" },
             { language: "Sinhala", quality: "S2 | 720p", size: "400MBx22", url: "https://mega.nz/folder/V01wxa7S#Buw_JHqI6Oyk-2viLba9DQ" }
         ]
     },
+    "4686": {
+        type: "tv",
+        title: "Ben 10 (2005)",
+        links: [
+            { language: "Sinhala", quality: "720p", size: "400MB", url: "https://t.me/sinhalaaaa/8358" }
+        ]
+    },
+    "6040": {
+        type: "tv",
+        title: "Ben 10: Alien Force (2008)",
+        links: [
+            { language: "Sinhala", quality: "S1 | 720p", size: "400MBx13", url: "https://drive.google.com/drive/folders/1-940paFjQb4BNxAU2SClU90ZtlE7wHTm" },
+            { language: "Sinhala", quality: "S2 | 720p", size: "400MBx13", url: "https://drive.google.com/drive/folders/1OIPYl_KlqHrbsbr7DcwfLY_-Pe-jya4d" },
+            { language: "Sinhala", quality: "S3 | 720p", size: "400MBx20", url: "https://drive.google.com/drive/folders/1alqQyxx5kacfVlKq9Kzi-7M7xdujVJaK" }
+        ]
+    },
+    "31109": {
+        type: "tv",
+        title: "Ben 10: Ultimate Alien (2010)",
+        links: [
+            { language: "Sinhala", quality: "S1 | 720p", size: "400MB", url: "https://t.me/sinhalaaaa/6742" },
+            { language: "Sinhala", quality: "S2 | 720p", size: "400MB", url: "https://t.me/sinhalaaaa/6784" },
+            { language: "Sinhala", quality: "S3 | 720p", size: "400MB", url: "https://t.me/sinhalaaaa/6798" }
+        ]
+    },
+    "46922": {
+        type: "tv",
+        title: "Ben 10: Omniverse (2012)",
+        links: [
+            { language: "Sinhala", quality: "S1 | 720p", size: "400MB", url: "https://t.me/sinhalaaaa/6858" },
+            { language: "Sinhala", quality: "S2 | 720p", size: "400MB", url: "https://t.me/sinhalaaaa/6867" },
+            { language: "Sinhala", quality: "S3 | 720p", size: "400MB", url: "https://t.me/sinhalaaaa/6877" },
+            { language: "Sinhala", quality: "S4 | 720p", size: "400MB", url: "https://t.me/sinhalaaaa/6887" }
+        ]
+    },
+    "19995": {
+        type: "movie",
+        title: "Avatar (2009)",
+        links: [
+            { language: "Sinhala", quality: "480p x264", size: "847 MB", url: "https://mega.nz/file/pzRzyCrD#PeufHUlMMMJ-DR2rudZ6ISYsvjaZRg3kjFYETdKXAi8" },
+             { language: "Sinhala", quality: "720p x264", size: "2.26 GB", url: "https://t.me/sinhalaaaa/7549" },
+             { language: "Sinhala", quality: "1080p x265 10-bit", size: "3.47 GB", url: "https://server-dubhublk.cloudew.workers.dev/1:/Remux/Dub/Avatar.2009.1080p.10bit.UHD.BluRay.x265.2CH.HEVC-DubZoneLK.mkv" },
+             { language: "Sinhala", quality: "1080p x265 10-bit HDR", size: "5.16 GB", url: "https://server-dubhublk.cloudew.workers.dev/1:/Remux/Dub/Avatar.2009.1080p.10bit.UHD.BluRay.x265.HDR.2CH-DubZoneLK.mkv" },
+             { language: "Sinhala", quality: "2160p x265 10-bit HDR", size: "12.25 GB", url: "https://server-dubhublk.cloudew.workers.dev/1:/Remux/Dub/%20Avatar.2009.2160p.10bit.UHD.BluRay.x265.HDR.2CH-DubZoneLK.mkv" }
+        ]
+    },
      "671": {
         type: "movie",
-        title: "Harry Potter",
+        title: "Harry Potter and the Philosopher's Stone (2001)",
         links: [
             { language: "Sinhala", quality: "720p x264", size: "1.39GB", url: "https://server-dubhublk.cloudew.workers.dev/1:/Remux/Dub/Harry.Potter.2001.720p.x264-DubZoneLK.mp4" },
              { language: "Sinhala", quality: "1080p 10bits x265 HDR DV", size: "2.23GB", url: "https://server-dubhublk.cloudew.workers.dev/1:/Remux/Dub/Harry.Potter.and.the.Sorcerer's.Stone.2001.1080p.UHD.BluRay.x265.HDR-DubZoneLK.mkv" },
               { language: "Hindi", quality: "720p", size: "1.9GB", url: "https://qw847lk.cloudatacdn.com/u5kj6tmxelb3sdgge6o46osbir7t4ulqsp7osjgfaxlpw76rsg3kz4guaeca/onidjxhpzl~RwVE48KX0k?token=ok6878nnt2bdglncb0omkxgx&expiry=1772177692912" }
         ]
     },
+    "672": {
+        type: "movie",
+        title: "Harry Potter and the Chamber of Secrets (2002)",
+        links: [
+            { language: "Sinhala", quality: "720p x264 1CH", size: "1.43GB", url: "https://server-dubhublk.cloudew.workers.dev/1:/Remux/Dub/Harry.Potter.2002.720p.x264-DubZoneLK.mp4" },
+              { language: "Sinhala", quality: "1080p 10bits x265 HDR DV", size: "3.14GB", url: "https://server-dubhublk.cloudew.workers.dev/1:/Remux/Dub/Harry.Potter.and.the.Chamber.of.Secrets.2002.1080p.UHD.BluRay.x265.HDR-DubZoneLK.mkv" }
+        ]
+    },
     "324857": {
         type: "movie",
-        title: "Spider-Man: Into the Spider-Verse",
+        title: "Spider-Man: Into the Spider-Verse (2018)",
         links: [
             { language: "Sinhala", quality: "480p - ITN", size: "490MB", url: "https://drive.google.com/file/d/10M2i5P7GNQt2Uh_Fclra0sV2paIyt7Bb/view?usp=drivesdk" },
               { language: "Sinhala", quality: "BluRay 720p x265 10bit - Sirasa TV", size: "1.40 GB", url: "https://server-dubhublk.cloudew.workers.dev/1:/Remux/Dub/Spider.Man.Into.The.Spider.Verse.2018.720p.10bit.BluRay.x265.HEVC-DubZoneLK.mkv" },
               { language: "Sinhala", quality: "BluRay 1080p x265 10bit - Sirasa TV", size: "2.76 GB", url: "https://server-dubhublk.cloudew.workers.dev/1:/Remux/Dub/Spider.Man.Into.The.Spider.Verse.2018.1080p.10bit.BluRay.x265.HEVC-DubZoneLK.mkv" },
               { language: "Sinhala", quality: "BluRay 2160p x265 10bit HDR - Sirasa TV", size: "5.73 GB", url: "https://server-dubhublk.cloudew.workers.dev/1:/Remux/Dub/Spider.Man.Into.The.Spider.Verse.2018.2160p.HDR.10bit.BluRay.x265.HEVC-DubZoneLK.mkv" }
         ]
+    },  "569094": {
+        type: "movie",
+        title: "Spider-Man: Across the Spider-Verse (2023)",
+        links: [
+              { language: "Sinhala", quality: "BluRay 720p x265 10bit - Sirasa TV", size: "1.86 GB", url: "https://server-dubhublk.cloudew.workers.dev/1:/Remux/Dub/Spider.Man.Across.The.Spider.Verse.2023.720p.10bit.BluRay.x265.HEVC-DubZoneLK.mkv" },
+              { language: "Sinhala", quality: "BluRay 1080p x265 10bit - Sirasa TV", size: "4.14 GB", url: "https://server-dubhublk.cloudew.workers.dev/1:/Remux/Dub/Spider.Man.Across.The.Spider.Verse.2023.1080p.10bit.BluRay.x265.HEVC-DubZoneLK.mkv" },
+              { language: "Sinhala", quality: "BluRay 2160p x265 10bit HDR - Sirasa TV", size: "7.94 GB", url: "https://server-dubhublk.cloudew.workers.dev/1:/Remux/Dub/Spider.Man.Across.The.Spider.Verse.2023.2160p.HDR.10bit.BluRay.x265.HEVC-DubZoneLK.mkv" }
+        ]
+    },
+    "673": {
+        type: "movie",
+        title: "Harry Potter and the Prisoner of Azkaban (2004)",
+        links: [
+            { language: "Sinhala", quality: "720p x264", size: "1.23 GB", url: "https://server-dubhublk.cloudew.workers.dev/1:/Remux/Dub/Harry.Potter.2004.720p.x264-DubZoneLK.mp4" },
+              { language: "Sinhala", quality: "1080p 10bits x265 HDR DV", size: "2.90 GB", url: "https://server-dubhublk.cloudew.workers.dev/1:/Remux/Dub/Harry.Potter.and.the.Prisoner.of.Azkaban.2004.1080p.UHD.BluRay.x265.HDR-DubZoneLK.mkv" }
+        ]
+    },
+    "674": {
+        type: "movie",
+        title: "Harry Potter and the Goblet of Fire (2005)",
+        links: [
+            { language: "Sinhala", quality: "720p x264", size: "870 MB", url: "https://cloud.sinhalachr.workers.dev/15:/Harry%20Potter%20Sinhala%20Dubbed/4%20-%20Harry%20Potter%20and%20the%20Goblet%20of%20Fire%20(2005)%20@sinhalafilmsgo.mp4" },
+              { language: "Sinhala", quality: "720p 10bits x265 HDR DV", size: "1.55 GB", url: "https://mega.nz/file/TklWXKxD#dgpxpG4FbPWQ_v-Ss8vQ_-OmqdO_5VJsQ052sjBg2AQ" }
+        ]
+    },
+    "675": {
+        type: "movie",
+        title: "Harry Potter and the Order of the Phoenix (2007)",
+        links: [
+            { language: "Sinhala", quality: "1080p x264", size: "1.3 GB", url: "https://cloud.sinhalachr.workers.dev/15:/Harry%20Potter%20Sinhala%20Dubbed/5%20-%20Harry%20Potter%20and%20the%20Order%20of%20the%20Phoenix%20(2007)%20@sinhalafilmsgo.m4v" },
+              { language: "Sinhala", quality: "720p 10bits x265 HDR DV", size: "1.55 GB", url: "https://files.cartoons.lk/430/Harry%20Potter%205%20%282007%29%20Cartoon.lk-720p.mp4" }
+        ]
     },
     "808": {
         type: "movie",
-        title: "Shrek",
+        title: "Shrek (2001)",
         links: [
             { language: "Sinhala", quality: "1080p x265 10-Bits", size: "1.36 GB", url: "https://server-dubhublk.cloudew.workers.dev/1:/Remux/Dub/Shrek.2001.1080p.10bit.BluRay.x265.HEVC-DubZoneLK.mkv" },
-              { language: "Sinhala", quality: "1080p x265 10-bit HDR", size: "2.08GB", url: "https://server-dubhublk.cloudew.workers.dev/1:/Remux/Dub/Shrek.2001.1080p.10bit.UHD.BluRay.x265.HDR.HEVC-DubZoneLK.mkv" }
+              { language: "Sinhala", quality: "1080p x265 10-bit HDR", size: "2.08GB", url: "https://server-dubhublk.cloudew.workers.dev/1:/Remux/Dub/Shrek.2001.1080p.10bit.UHD.BluRay.x265.HDR.HEVC-DubZoneLK.mkv" },
+              { language: "Hindi", quality: "720p", size: "1GB", url: "https://cmlhz.com/movies-xxx/disk2/movies/Shrek.1.mp4" }
         ]
     },
     "308697": {
@@ -77,14 +163,38 @@ const DUBBED_REGISTRY = {
      
     "413881": {
         type: "movie",
-        title: "According to Matthew",
+        title: "According to Matthew (2018)",
         links: [
             { language: "Sinhala", quality: "480p", size: "390.3 MB", url: "https://t.me/sinhalaaaa/5566" }
         ]
     },
+    "65143": {
+        type: "tv",
+        title: "Descendants of the Sun (2016)",
+        links: [
+            { language: "Sinhala", quality: "720p", size: "500MB x 16", url: "https://t.me/sinhalaaaa/10264" }
+        ]
+    },
+    "615453": {
+        type: "movie",
+        title: "Ne Zha (2019)",
+        links: [
+            { language: "Sinhala", quality: "480p", size: "540MB", url: "https://t.me/sinhalaaaa/10987" },
+            { language: "Sinhala", quality: "720p", size: "980MB", url: "https://t.me/sinhalaaaa/10986" },
+            { language: "Sinhala", quality: "1080p", size: "1.8GB", url: "https://t.me/sinhalaaaa/10984" }
+        ]
+    },
+    "811634": {
+        type: "movie",
+        title: "The Monkey King: Reborn (2021)",
+        links: [
+            { language: "Sinhala", quality: "720p", size: "870MB", url: "https://t.me/sinhalaaaa/11573" },
+             { language: "Sinhala", quality: "1080p", size: "2GB", url: "https://t.me/sinhalaaaa/11572" }
+        ]
+    },
     "149870": {
         type: "movie",
-        title: "The Wind Rises",
+        title: "The Wind Rises (2013)",
         links: [
             { language: "Sinhala", quality: "720p x264", size: "1.16GB", url: "https://server-dubhublk.cloudew.workers.dev/1:/Remux/Dub/The.Wind.Rises.2013.720p.BluRay.x264-DubZoneLK.mkv" },
             { language: "Sinhala", quality: "1080p x265 10-bits", size: "1.9GB", url: "https://server-dubhublk.cloudew.workers.dev/1:/Remux/Dub/The.Wind.Rises.2013.1080p.10bit.BluRay.x265.HEVC-DubZoneLK.mkv" },
@@ -93,7 +203,7 @@ const DUBBED_REGISTRY = {
     },
      "629542": {
         type: "movie",
-        title: "The Bad Guys",
+        title: "The Bad Guys (2022)",
         links: [
             { language: "Sinhala", quality: "720p x265 10-bits", size: "660 MB", url: "https://mega.nz/file/KEp0TZTQ#8-iONBZpVwVRN1UxOIMaydWZmh9tld8RI9zD0v4jTYw" },
             { language: "Sinhala", quality: "1080p x265 10-bits | SIN-1.0 + ENG-7.1", size: "1.7GB", url: "https://server-dubhublk.cloudew.workers.dev/1:/Remux/Dub/The.Bad.Guys.2022.1080p.10bit.BluRay.SI-1.0+EN-7.1.x265.HEVC-DubZoneLK.mkv" },
@@ -102,12 +212,60 @@ const DUBBED_REGISTRY = {
     },
     "527729": {
         type: "movie",
-        title: "Asterix : The Secret of the Magic Potion",
+        title: "Asterix : The Secret of the Magic Potion (2018)",
         links: [
         
-            { language: "Sinhala", quality: "480p - ITN", size: "1.7GB", url: "https://drive.google.com/file/d/1g1CTnk0g6qZtx_dDuMRVNDZXOXhdezTk/view?usp=drivesdk" },
+            { language: "Sinhala", quality: "480p - ITN", size: "400 MB", url: "https://drive.google.com/file/d/1g1CTnk0g6qZtx_dDuMRVNDZXOXhdezTk/view?usp=drivesdk" },
               { language: "Sinhala", quality: "720p", size: "1.05GB", url: "https://t.me/sinhalaaaa/8488" },
             { language: "Sinhala", quality: "1080p x264 ENG-5.1CH + SIN-1CH - Sirasa TV", size: "1.85 GB", url: "https://server-dubhublk.cloudew.workers.dev/1:/Remux/Dub/Asterix.The.Secret.of.the.Magic.Potion.2018.EN-SI.1080p.10bit.Bluray.1CH.x265.HEVC-DubZoneLK.mkv" }
+        ]
+    },
+    "170522": {
+        type: "movie",
+        title: "Asterix: The Mansions of the Gods (2014)",
+        links: [
+        
+            { language: "Sinhala", quality: "480p - ITN", size: "400 MB", url: "https://drive.google.com/file/d/1FV4lW-6u77_slhM0L-LWs3iCrkgy_IaV/view?usp=drivesdk" },
+              { language: "Sinhala", quality: "720p x265 - Sirasa TV", size: "625 MB", url: "https://mega.nz/file/KlZWTQzQ#R3T1AJ_O3XuYPoJbu4JWVSdy2t06vQrcMasHiiL7CPA" },
+            { language: "Sinhala", quality: "1080p x264 - Sirasa TV", size: "1.78 GB", url: "https://mega.nz/file/WlgyFK6D#hUkP5Q5EdOmA-qTuhzMPDX35l0pCHXrY3s52-QFYzYs" }
+        ]
+    },
+    "86310": {
+        type: "tv",
+        title: "RadhaKrishn",
+        links: [
+            { language: "Sinhala", quality: "480p", size: "≈200MB", url: "https://col3negoriginal.lk/page/morevideoForName/305/Krishna" }
+        ]
+    },
+    "560050": {
+        type: "movie",
+        title: "Over the Moon (2020)",
+        links: [
+            { language: "Sinhala", quality: "720p", size: "900MB", url: "https://cloud.sinhalachr.workers.dev/17:/Over.The.Moon.2020.720p.WEBRip.x264-@sinhalafilmsgo.mkv" },
+            { language: "Sinhala", quality: "1080p", size: "1.3BG", url: "https://cloud.sinhalachr.workers.dev/17:/Over.the.Moon.2020.1080p.10bit.WEBRip.x265.HEVC-@sinhalafilmsgo.mkv" }
+        ]
+    },
+    "881201": {
+        type: "movie",
+        title: "Gajaman (2023)",
+        links: [
+            { language: "Sinhala", quality: "720p", size: "1 GB", url: "https://edge2-waw-sprintcdn.r66nv9ed.com/download/02/04475/fxzs5lxybph3_h/Gajaman_THILINA_TECH.mp4?t=hdqJiFsQR3J1biWG1S5m6ypHWPjIcADRhjJSyY2wQtU&s=1772198693&e=10800&f=22379925&srv=1050&asn=&sp=4000" }
+        ]
+    },
+    "65334": {
+        type: "tv",
+        title: "Miraculous: Tales of Ladybug & Cat Noir (2015)",
+        links: [
+            { language: "Sinhala", quality: "480p", size: "400 MB", url: "https://ginisisilacartoon.net/more.php?cat=869" },
+              { language: "Sinhala", quality: "480p", size: "S2 | 400 MB", url: "https://t.me/sinhalaaaa/8996" }
+        ]
+    },
+    "109445": {
+        type: "movie",
+        title: "Frozen (2013)",
+        links: [
+            { language: "Sinhala", quality: "1080p", size: "1.8GB", url: "https://drive.usercontent.google.com/download?id=1Mz22qYMosVHlNViuww1-NAEWpupAWbiv&export=download&authuser=0" },
+              { language: "Sinhala", quality: "720p", size: "809.45MB", url: "https://files.cartoons.lk/372/Frozen%20Cartoons.lk.mp4" }
         ]
     }
 };
