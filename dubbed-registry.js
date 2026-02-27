28de8afa395f3d4c821a4ab2bc1c53964598f0c3
Chithruka Sri Yudam Nanayakkara <chithrukasri@gmail.com>
2026-02-27 13:08:33 +0530

    Add files via upload

--- dubbed-registry.js
+++ dubbed-registry.js
@@ -4,14 +4,16 @@ const DUBBED_REGISTRY = {
         type: "movie", // Specify if it's a movie
         title: "Titanic",
         links: [
-            { language: "Sinhala Dub", quality: "720p", size: "1.2GB", url: "https://server-dubhublk.cloudew.workers.dev/1:/Remux/Dub/Titanic.1997.Open.Matte.720p.BluRay.x264-DubZoneLK.mkv" }
+            { language: "Sinhala", quality: "720p", size: "1.2GB", url: "https://server-dubhublk.cloudew.workers.dev/1:/Remux/Dub/Titanic.1997.Open.Matte.720p.BluRay.x264-DubZoneLK.mkv" },
+            { language: "Sinhala", quality: "1080p", size: "2.1GB", url: "https://server-dubhublk.cloudew.workers.dev/1:/Remux/Dub/Titanic.1997.Open.Matte.1080p.BluRay.x264.2CH-DubZoneLK.mkv" },
+              { language: "Hindi", quality: "720p", size: "1.9GB", url: "https://qw847lk.cloudatacdn.com/u5kj6tmxelb3sdgge6o46osbir7t4ulqsp7osjgfaxlpw76rsg3kz4guaeca/onidjxhpzl~RwVE48KX0k?token=ok6878nnt2bdglncb0omkxgx&expiry=1772177692912" }
         ]
     },
-    "60625": {
-        type: "tv", // Specify if it's a TV show (Example: Rick and Morty)
-        title: "Rick and Morty",
+    "7704": {
+        type: "tv",
+        title: "Legend of the Seeker",
         links: [
-            { language: "Sinhala Dub", quality: "1080p", size: "400MB", url: "..." }
+            { language: "Sinhala", quality: "720p", size: "400MBx22", url: "https://mega.nz/folder/goNXGAzJ#RunQgaWWLOeCv2CyOuCnRg" }
         ]
     }
 };
