1d93dd273db010ab3deec26e2bd7868ea20a4cb8
Chithruka Sri Yudam Nanayakkara <chithrukasri@gmail.com>
2026-03-12 23:58:59 +0530

    Add files via upload

---
+++ YouTube/data.js
@@ -0,0 +1,42 @@
+const videos = [
+  {
+    id: "v1",
+    title: "Big Buck Bunny",
+    description: "Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself.",
+    sources: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+    thumb: "https://i.ytimg.com/vi/aqz-KE-bpKQ/maxresdefault.jpg",
+    category: "Movies",
+    channel: "Blender Foundation",
+    views: "24M Views"
+  },
+  {
+    id: "v2",
+    title: "Elephant Dream",
+    description: "The first Blender Open Movie from 2006.",
+    sources: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+    thumb: "https://storage.googleapis.com/gtv-videos-bucket/sample/images/ElephantsDream.jpg",
+    category: "Movies",
+    channel: "Blender Foundation",
+    views: "12M Views"
+  },
+  {
+    id: "v3",
+    title: "For Bigger Blazes",
+    description: "HBO GO now works with Chromecast.",
+    sources: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+    thumb: "https://storage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerBlazes.jpg",
+    category: "Coding",
+    channel: "Google",
+    views: "5M Views"
+  },
+  {
+    id: "v4",
+    title: "Tears of Steel",
+    description: "Tears of Steel, HLS with IMSC Captions.",
+    sources: "https://test-streams.mux.dev/tos_ismc/main.m3u8",
+    thumb: "https://m.media-amazon.com/images/S/pv-target-images/ac878c08d94a3da94048c54c4585013f2c7672c33ef5236307eba9fb8ae96d38.png",
+    category: "Movies",
+    channel: "Blender Foundation",
+    views: "1.5M Views"
+  }
+];
