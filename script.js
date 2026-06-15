1a1bdc81fb1a66107253901db92fa014a27652ae
Chithruka Sri Yudam Nanayakkara <chithrukasri@gmail.com>
2026-06-15 11:31:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ let currentServerIndex = 0;
 const BASE_SERVER_URLS = [
     { name: "Server 1", movie: "https://vidrock.ru/movie/[ID]", tv: "https://vidrock.ru/tv/[ID]/[S]/[E]" },
     { name: "Server 2", movie: "https://player.videasy.net/movie/[ID]", tv: "https://player.videasy.net/tv/[ID]/[S]/[E]?nextEpisode=true&episodeSelector=true" },
-    { name: "Server 1", movie: "https://vidlink.pro/movie/[ID]?primaryColor=E50914&secondaryColor=221F1F&iconColor=eefdec&icons=default&player=default&title=true&poster=true&autoplay=false&sub_label=English&fallback_url=https://streamimdb.ru/embed/movie/[ID]", tv: "https://vidlink.pro/tv/[ID]/[S]/[E]?primaryColor=E50914&secondaryColor=221F1F&iconColor=eefdec&icons=default&player=default&title=true&poster=true&autoplay=false&nextbutton=true&fallback_url=https://streamimdb.ru/embed/tv/[ID]" },
+    { name: "Server 3", movie: "https://vidlink.pro/movie/[ID]?primaryColor=E50914&secondaryColor=221F1F&iconColor=eefdec&icons=default&player=default&title=true&poster=true&autoplay=false&sub_label=English&fallback_url=https://streamimdb.ru/embed/movie/[ID]", tv: "https://vidlink.pro/tv/[ID]/[S]/[E]?primaryColor=E50914&secondaryColor=221F1F&iconColor=eefdec&icons=default&player=default&title=true&poster=true&autoplay=false&nextbutton=true&fallback_url=https://streamimdb.ru/embed/tv/[ID]" },
    { name: "Server 4", movie: "https://www.vidsrc.wtf/4/movie/[ID]?color=e01621", tv: "https://www.vidsrc.wtf/4/tv/[ID]/[S]/[E]?color=e01621" },
    { name: "Server 5", movie: "https://1embed.cc/embed/movie/[ID]", tv: "https://1embed.cc/embed/tv/[ID]/[S]/[E]" }
 ];
